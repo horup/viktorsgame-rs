b8d1440798246f9eb8bb7a02d561573df2eaff0f
= <horup@deepstacker.com>
2024-02-12 21:30:21 +0100

    wip

--- bevy_webserver/src/lib.rs
+++ bevy_webserver/src/lib.rs
@@ -1,11 +1,15 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, reflect::erased_serde::Serialize};
+use serde::de::DeserializeOwned;
 use std::marker::PhantomData;
+
+pub trait Message : Send + Sync + Serialize + DeserializeOwned + 'static {}
+impl<T> Message for T where T : Send + Sync + Serialize + DeserializeOwned + 'static {}
 
 pub struct BevyWebserver<T> {
     pub phantom:PhantomData<T>
 }
 
-impl<T : Send + Sync + 'static> Plugin for BevyWebserver<T> {
+impl<T : Message> Plugin for BevyWebserver<T> {
     fn build(&self, app: &mut App) {
         
     }
